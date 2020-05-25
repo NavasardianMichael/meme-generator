eb09934d4f015e3cdd3c740909512e8d64b679c7
Navasardian Michael <navasardianmichael@gmail.com>
2020-05-26 01:35:18 +0400

    Updates

---
+++ precache-manifest.3ad6262b22598f2c9347dcb457acf6ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f12bca05c58bf99923c4ca93740015f9",
+    "url": "/meme-generator/index.html"
+  },
+  {
+    "revision": "6ab3154d6b43b93bc7b5",
+    "url": "/meme-generator/static/css/main.b0f6a8fa.chunk.css"
+  },
+  {
+    "revision": "fb536641eb9084ce5c37",
+    "url": "/meme-generator/static/js/2.9d958aa2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/meme-generator/static/js/2.9d958aa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ab3154d6b43b93bc7b5",
+    "url": "/meme-generator/static/js/main.1be3bc7b.chunk.js"
+  },
+  {
+    "revision": "5fae4ce01ef1a8c8c6ee",
+    "url": "/meme-generator/static/js/runtime-main.4c6e3781.js"
+  }
+]);
